849677322761ab585ac6313a523d37093f9a089b
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-06-02 11:21:14 +0200

    Update retweet.py

--- retweet.py
+++ retweet.py
@@ -4,3 +4,5 @@ class Retweet(Tweet):
     def __init__(self, message, sender, tweet):
         super().__init__(message, sender)
         self.retweeted_tweet = tweet
+    def __str__(self):
+            return f"Retweet from {self.sender.username} at {self.time}: {self.message}\nOriginal Tweet: {self.retweeted_tweet}"
