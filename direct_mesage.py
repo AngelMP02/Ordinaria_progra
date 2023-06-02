c83b27946404d188ce6c037c2d3600df5218ca08
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-06-02 11:16:19 +0200

    Delete direct_mesage.py

--- direct_mesage.py
+++
@@ -1,6 +0,0 @@
-from tweet import Tweet
-
-class DirectMessage(Tweet):
-    def __init__(self, message, sender, receiver):
-        super().__init__(message, sender)
-        self.receiver = receiver
