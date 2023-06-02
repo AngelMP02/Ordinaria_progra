0b2d545fe971d608b1c5b29c0193b9f59c3d4bc5
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-06-02 11:22:17 +0200

    Update direct_message.py

--- direct_message.py
+++ direct_message.py
@@ -4,3 +4,5 @@ class DirectMessage(Tweet):
     def __init__(self, message, sender, receiver):
         super().__init__(message, sender)
         self.receiver = receiver
+    def __str__(self):
+        return f"Direct Message from {self.sender.username} to {self.receiver.username} at {self.time}: {self.message}"
