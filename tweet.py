ba74691ccb43ed76b59e14343fcd16b40b66840b
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-06-02 11:18:15 +0200

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
 class Tweet:
+    MAX_CHARACTERS = 140
+
     def __init__(self, message, sender):
+        if len(message) > self.MAX_CHARACTERS:
+            raise ValueError("El mensaje excede los 140 caracteres.")
         self.message = message
         self.sender = sender
         self.time = datetime.now()
